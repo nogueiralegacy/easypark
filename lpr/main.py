65f8219b6062017cd4142f0a496efe1925799528
soft-felipe <felipem791@gmail.com>
2024-12-19 12:24:46 -0300

    feat: add semaforo no produtor (nao é o melhor mas)

--- lpr/main.py
+++ lpr/main.py
@@ -6,15 +6,18 @@ import time
 
 def main():
     cam = cv2.VideoCapture(0)
-    
+
+    # A melhor proposta aqui seria utilizar o Redis com armazenamento temporário
+    last_plate = None  # Armazena a última placa enviada
     while True:
         ret, frame = cam.read()
         
         placa = process_plate(frame)
         
-        if placa is not None:
+        if placa is not None and placa != last_plate:
             print('PLACA ENVIADA: ', placa)
             send_and_halt(plate=placa)
+            last_plate = placa  # Atualiza a última placa enviada
 
         if cv2.waitKey(1) == ord('q'):
             break

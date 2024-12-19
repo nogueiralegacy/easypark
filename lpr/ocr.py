522df34b98a2f97903e818c178cb1afbfe2a7e31
Lucas Bernardes <lucasbernardes24@gmail.com>
2024-12-19 10:00:49 -0300

    test

--- lpr/ocr.py
+++ lpr/ocr.py
@@ -19,24 +19,31 @@ def process_plate(image):
         if perimeter > 150:
             aprox = cv2.approxPolyDP(c, 0.03 * perimeter, True)
             if len(aprox) == 4:
-                (x,y,h,w) = cv2.boundingRect(c)
-                if h > w * 2.5:
-                    cv2.rectangle(image, (x,y), (x + h, y + w), (0,255,0), 2)
-                    roi = img[y:y+w, x:x+h]
+                (x,y,w,h) = cv2.boundingRect(c)
+                if w > h * 2.5:
+                    cv2.rectangle(image, (x,y), (x + w, y + h), (0,255,0), 2)
 
-    cv2.imshow('camera', image)
+                    #DESENHAR OS RETANGULOS NA IMAGEM COM FILTRO
+                    cv2.rectangle(img, (x,y), (x + w, y + h), (0,255,0), 2)
+                    
+                    roi = img[y:y+h, x:x+w]
+
+                    if roi is not None:
+                        custom_config = r'-c tessedit_char_blacklist=abcdefghijklmnopqrstuvwxyz/ --psm 6'
+                        plate = (pytesseract.image_to_string(roi, config=custom_config))
+                        pattern = re.compile('[\W_]+')
+                        plate = pattern.sub('', plate)
+                        print('LEITURA DO ALGORITMO: ', plate)
+                        
+                        if(validate_plate(plate)):
+                            return plate
+
+    #COMENTA A QUE NAO FOR USAR
+    #cv2.imshow('camera normal', image)
     
-    if roi is not None:
-        custom_config = r'-c tessedit_char_blacklist=abcdefghijklmnopqrstuvwxyz/ --psm 6'
-        plate = (pytesseract.image_to_string(roi, config=custom_config))
-        pattern = re.compile('[\W_]+')
-        plate = pattern.sub('', plate)
-        
-        if(validate_plate(plate)):
-            return plate    
-        
-        return None
-
+    #IMAGEM
+    cv2.imshow('camera com filtros', img)
+    
     return None
 
 def validate_plate(plate):
